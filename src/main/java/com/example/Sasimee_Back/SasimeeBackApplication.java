d36cbcf49ef3c25bf5947f54215c168afdfce1c3
Lee-jong-won <dlwhddnjs951@naver.com>
2024-12-02 00:01:24 +0900

    chore : 초기 설정 및 도커 파일 추가

---
+++ src/main/java/com/example/Sasimee_Back/SasimeeBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.Sasimee_Back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SasimeeBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SasimeeBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Sasimee_Back/SasimeeBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Sasimee_Back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SasimeeBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
