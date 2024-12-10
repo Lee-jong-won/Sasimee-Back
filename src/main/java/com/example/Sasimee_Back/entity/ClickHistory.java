6334a8a7a7f28ce6a8986ba0dcc6b20feecd8081
hashedbrown01 <swanjsj@naver.com>
2024-12-10 12:32:38 +0900

    feat : ClickHistory 및 추천 기능 추가

---
+++ src/main/java/com/example/Sasimee_Back/entity/ClickHistory.java
@@ -0,0 +1,23 @@
+package com.example.Sasimee_Back.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class ClickHistory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    @ManyToOne
+    private Post post;
+
+    @ManyToOne
+    private User user;
+
+    private String timestamp;
+}
