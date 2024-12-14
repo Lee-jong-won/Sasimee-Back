7c596450723cd6ed12af55ff731ec999cc0754d2
hashedbrown01 <swanjsj@naver.com>
2024-12-14 11:24:22 +0900

    feat : Gemini를 통한 AI 검증 로직 추가

---
+++ src/main/java/com/example/Sasimee_Back/dto/GeminiDTO.java
@@ -0,0 +1,67 @@
+package com.example.Sasimee_Back.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+public class GeminiDTO {
+
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class GeminiRequest{
+        private List<Content> contents;
+
+        @Data
+        public class Content {
+            private List<Part> parts;
+
+            public Content(String text){
+                parts = new ArrayList<>();
+                Part part = new Part(text);
+                parts.add(part);
+            }
+
+            @Data
+            @NoArgsConstructor
+            @AllArgsConstructor
+            public class Part{
+                private String text;
+            }
+        }
+
+        public void createGeminiRequest(String text){
+            this.contents = new ArrayList<>();
+            Content content = new Content(text);
+            contents.add(content);
+        }
+    }
+
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class GeminiResponse{
+        private List<Candidate> candidates;
+
+        @Data
+        public static class Candidate{
+            private Content content;
+            private String finishReason;
+        }
+
+        @Data
+        public static class Content{
+            private List<Part> parts;
+            private String role;
+        }
+
+        @Data
+        public static class Part{
+            private String text;
+        }
+    }
+}
