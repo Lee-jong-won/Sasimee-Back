bd4a5357787cbaf70f7ef31b83ef65448e4b3450
hashedbrown01 <swanjsj@naver.com>
2024-12-15 22:27:11 +0900

    fix : Survey와 Task 관련 Enum 타입의 JSON 직렬화

--- src/main/java/com/example/Sasimee_Back/entity/PostType.java
+++ src/main/java/com/example/Sasimee_Back/entity/PostType.java
@@ -6,24 +6,26 @@ import lombok.Getter;
 
 @Getter
 public enum PostType {
-    S("Survey"), T("Task");
+    S("Survey"),
+    T("Task");
 
     private final String postType;
 
-    PostType(String postType) {this.postType = postType;}
+    PostType(String postType) {
+        this.postType = postType;
+    }
 
     @JsonValue
     public String getPostType() {return postType;}
 
     @JsonCreator
     public static PostType fromString(String value) {
-        if("Survey".equalsIgnoreCase(value)) {
-            return S;
-        } else if ("Task".equalsIgnoreCase(value)) {
-            return T;
-        }else{
-            throw new IllegalArgumentException("Invalid post type value: " + value);
+        for(PostType t : PostType.values()) {
+            if(t.postType.equalsIgnoreCase(value)) {
+                return t;
+            }
         }
+        throw new IllegalArgumentException("Invalid post type value : " + value);
     }
 
 
