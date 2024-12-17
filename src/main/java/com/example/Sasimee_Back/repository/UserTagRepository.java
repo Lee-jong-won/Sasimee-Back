8796fc83e8fa8ff5f08acc1b3ab4c4f7122173a6
hashedbrown01 <swanjsj@naver.com>
2024-12-17 15:33:40 +0900

    fix : userTagRepository findByUser추가

--- src/main/java/com/example/Sasimee_Back/repository/UserTagRepository.java
+++ src/main/java/com/example/Sasimee_Back/repository/UserTagRepository.java
@@ -5,11 +5,13 @@ import com.example.Sasimee_Back.entity.User;
 import com.example.Sasimee_Back.entity.UserTag;
 import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface UserTagRepository extends JpaRepository<UserTag, Long> {
     Optional<UserTag> findByNameAndCategory(String name, TagCategory category);
-    List<UserTag> findByUsers(User user);
+    @Query("SELECT ut FROM UserTag ut JOIN ut.users u WHERE u = :user")
+    List<UserTag> findByUser(@Param("user") User user);
 }
