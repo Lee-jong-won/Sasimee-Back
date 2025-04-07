9dfe01ccf2c5c63c0ea86b36115167eb8b0555a3
Lee-jong-won <78682445+Lee-jong-won@users.noreply.github.com>
2025-04-07 16:55:51 +0900

    chore : swagger config 파일 수정

--- src/main/java/com/example/Sasimee_Back/config/SwaggerConfig.java
+++ src/main/java/com/example/Sasimee_Back/config/SwaggerConfig.java
@@ -14,23 +14,10 @@ import java.util.List;
 @RequiredArgsConstructor
 public class SwaggerConfig {
 
-    //http://localhost:8080/swagger-ui.html
-
-    @Value("${sasimee.openapi.dev-url}")
-    private String devUrl;
-    @Value("${sasimee.openapi.prod-url}")
-    private String prodUrl;
-
+    //http://[server_ip]:8080/swagger-ui.html
 
     @Bean
     public OpenAPI openAPI() {
-        final Server devServer = new Server();
-        devServer.setUrl(devUrl);
-        devServer.description("개발 환경 서버 URL");
-
-        final Server prodServer = new Server();
-        prodServer.setUrl(prodUrl);
-        prodServer.description("운영 환경 서버 URL");
 
         final Info info = new Info()
                 .title("Sasimee API")
@@ -38,8 +25,7 @@ public class SwaggerConfig {
                 .description("사심이 API");
 
         return new OpenAPI()
-                .info(info)
-                .servers(List.of(devServer, prodServer));
+                .info(info);
     }
 
 }
